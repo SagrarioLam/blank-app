edb609d20bbb720ba12cd2352967f239716dec7e
SagrarioLam <492210442@utaltamira.edu.mx>
2025-11-20 20:01:32 -0600

    Add text input for user name in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+# 2. Nueva variable de texto
+nuevo_nombre = st.text_input(
+    'Introduce tu nombre (Nueva variable)', # Título del widget
+    'Escribe aquí' # Texto por defecto
